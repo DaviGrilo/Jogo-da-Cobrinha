356f059c9ac809f27070f989d6aac5d43d952cdf
DaviGrilo <145941516+DaviGrilo@users.noreply.github.com>
2024-07-02 10:36:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
+const scoreDisplay = document.getElementById('score');
 let gameInterval;
 
 let snake = [{ x: 10, y: 10 }];
@@ -23,7 +24,7 @@ function drawFood() {
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (!gameRunning) return; 
+  if (!gameRunning) return;
 
   let head = { x: snake[0].x, y: snake[0].y };
   if (direction === 'right') head.x++;
@@ -41,7 +42,7 @@ function update() {
   ) {
     gameRunning = false; 
     clearInterval(gameInterval); 
-    alert('Game over!');
+    alert(`Game Over! Sua pontuação final é: ${score}`);
 
     
     document.getElementById('restartButton').style.display = 'block';
@@ -49,6 +50,8 @@ function update() {
 
   if (snake[0].x === food.x && snake[0].y === food.y) {
     snakeSize++;
+    score += 10;
+    scoreDisplay.textContent = `Score: ${score}`;
     food = {
       x: Math.floor(Math.random() * (canvas.width / 20)),
       y: Math.floor(Math.random() * (canvas.height / 20)),
@@ -71,11 +74,14 @@ function handleInput(event) {
 document.addEventListener('keydown', handleInput);
 
 function startGame() {
+  
   document.getElementById('startButton').style.display = 'none';
   canvas.style.display = 'block';
 
   // Inicia o jogo
   gameRunning = true;
+  score = 0;
+  scoreDisplay.textContent = `Score: ${score}`;
   gameInterval = setInterval(update, 100);
 }
 
@@ -84,6 +90,8 @@ function restartGame() {
   snakeSize = 1;
   food = { x: 5, y: 5 };
   direction = 'right';
+  score = 0;
+  scoreDisplay.textContent = `Score: ${score}`;
   gameRunning = true;
   document.getElementById('restartButton').style.display = 'none';
   gameInterval = setInterval(update, 100); // Reinicia o intervalo de atualização do jogo
